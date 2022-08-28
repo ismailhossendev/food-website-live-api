558c0a1f2de3b790bbd79f038efa2d78ca86bf50
Ismail Hossen <ismailhossendev@gmail.com>
2022-08-28 19:49:30 +0600

    main api section added

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// api 
+const mainApi = () => {
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMain(data.meals));
+}
+// main section 
+const displayMain = data => {
+    const mainContainer = document.getElementById('main-container');
+    mainContainer.innerHTML = ``
+    for (const food of data) {
+        const foodIns = food.strInstructions.slice(0,100)
+        const cardDiv = document.createElement('div')
+        cardDiv.classList.add('col')
+        cardDiv.innerHTML = `
+        <div class="card h-100">
+        <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${food.strMeal}</h5>
+          <h6 class="card-title">Catagory: ${food.strCategory}</h6>
+          <p class="card-text">${foodIns}...</p>
+          <button class="btn btn-primary">Details</button>
+        </div>
+      </div>
+        `
+        mainContainer.appendChild(cardDiv)
+    }
+}
+mainApi()
